0b218827671148af8ab3623dc626817d9ad33306
Xin <804052545@qq.com>
2018-05-27 22:06:22 +0800

    主界面框架

--- app/src/main/java/com/axin/coffee/MainActivity/MainActivity.java
+++ app/src/main/java/com/axin/coffee/MainActivity/MainActivity.java
@@ -1,15 +1,31 @@
 package com.axin.coffee.MainActivity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import com.axin.coffee.coffee.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.btn_back)
+    Button btnBack;
+    @BindView(R.id.slideMenu)
+    SlideMenu slideMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //取消状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
+
+
 }
