9318e948436525f85acd9666522465611ef8b6c2
Xin <804052545@qq.com>
2018-06-04 13:50:59 +0800

    主界面框架

--- app/src/main/java/com/axin/coffee/MainActivity/MainActivity.java
+++ app/src/main/java/com/axin/coffee/MainActivity/MainActivity.java
@@ -1,22 +1,43 @@
 package com.axin.coffee.MainActivity;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
+import android.widget.TextView;
 
 import com.axin.coffee.coffee.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    @BindView(R.id.btn_back)
-    Button btnBack;
     @BindView(R.id.slideMenu)
     SlideMenu slideMenu;
+    @BindView(R.id.physical_exam_menu)
+    TextView physicalExamMenu;
+    @BindView(R.id.drink_drive_menu)
+    TextView drinkDriveMenu;
+    @BindView(R.id.medicine_lib_menu)
+    TextView medicineLibMenu;
+    @BindView(R.id.test_game_menu)
+    TextView testGameMenu;
+    @BindView(R.id.setting_menu)
+    TextView settingMenu;
+    @BindView(R.id.about_us_menu)
+    TextView aboutUsMenu;
+    @BindView(R.id.exit_login_menu)
+    TextView exitLoginMenu;
+
+    FragmentManager fm = getFragmentManager();
+    FragmentTransaction tran = fm.beginTransaction();
+    Main_Fragment f1=new Main_Fragment();
+    Test_game_Fragment f2=new Test_game_Fragment();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +46,36 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        tran.add(R.id.message, f1, "index").show(f1).add(R.id.message, f2, "bill").hide(f2);
+        tran.commit();
     }
 
 
+
+    @OnClick({R.id.physical_exam_menu, R.id.drink_drive_menu, R.id.medicine_lib_menu, R.id.test_game_menu, R.id.setting_menu, R.id.about_us_menu, R.id.exit_login_menu})
+    public void onViewClicked(View view) {
+
+        switch (view.getId()) {
+            case R.id.physical_exam_menu:
+                FragmentManager fm_1 = getFragmentManager();
+                FragmentTransaction tran_0 = fm_1.beginTransaction();
+                tran_0.show(f1).hide(f2).commit();
+                break;
+            case R.id.drink_drive_menu:
+                break;
+            case R.id.medicine_lib_menu:
+                break;
+            case R.id.test_game_menu:
+                FragmentManager fm_2 = getFragmentManager();
+                FragmentTransaction tran_1 = fm_2.beginTransaction();
+                tran_1.show(f2).hide(f1).commit();
+                break;
+            case R.id.setting_menu:
+                break;
+            case R.id.about_us_menu:
+                break;
+            case R.id.exit_login_menu:
+                break;
+        }
+    }
 }
--- app/src/main/java/com/axin/coffee/coffee/LoginActivity/LoginActivity.java
+++ app/src/main/java/com/axin/coffee/coffee/LoginActivity/LoginActivity.java
@@ -1,18 +1,31 @@
 package com.axin.coffee.coffee.LoginActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.VideoView;
 
+import com.axin.coffee.MainActivity.MainActivity;
+import com.axin.coffee.RegisterActivity.RsgisterActivity;
 import com.axin.coffee.coffee.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity implements MediaPlayer.OnPreparedListener, MediaPlayer.OnCompletionListener {
 
+    @BindView(R.id.sig_in)
+    Button sigIn;
+    @BindView(R.id.register)
+    TextView register;
     private VideoView mVideoView;
-
 
 
     @Override
@@ -21,6 +34,7 @@ public class LoginActivity extends AppCompatActivity implements MediaPlayer.OnPr
         //取消状态栏
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
         mVideoView = (VideoView) findViewById(R.id.videoView);
         initVideoView();
     }
@@ -28,7 +42,7 @@ public class LoginActivity extends AppCompatActivity implements MediaPlayer.OnPr
     private void initVideoView() {
         //设置屏幕常亮
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        mVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.mqr));
+        mVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.login_movie));
         //设置相关的监听
         mVideoView.setOnPreparedListener(this);
         mVideoView.setOnCompletionListener(this);
@@ -39,7 +53,7 @@ public class LoginActivity extends AppCompatActivity implements MediaPlayer.OnPr
     public void onPrepared(MediaPlayer mp) {
         //开始播放
         mVideoView.start();
-    //    mHandler.postDelayed(runnable, TIME);
+        //    mHandler.postDelayed(runnable, TIME);
     }
 
     //播放结束
@@ -47,6 +61,24 @@ public class LoginActivity extends AppCompatActivity implements MediaPlayer.OnPr
     public void onCompletion(MediaPlayer mp) {
         //开始播放
         mVideoView.start();
+    }
+
+
+    @OnClick({R.id.sig_in, R.id.register})
+    public void onViewClicked(View view) {
+        Intent intent = new Intent();
+        switch (view.getId()) {
+            case R.id.sig_in:
+                //Intent intent = new Intent();
+                //intent.putExtra("testIntent", "123");
+                intent.setClass(LoginActivity.this, MainActivity.class); //设置跳转的Activity
+                LoginActivity.this.startActivity(intent);
+                break;
+            case R.id.register:
+                intent.setClass(LoginActivity.this, RsgisterActivity.class); //设置跳转的Activity
+                LoginActivity.this.startActivity(intent);
+                break;
+        }
     }
 }
 
